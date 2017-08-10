41b8934b6b72d732122fdf4549ee130f38da179b
m4mohaiman <m4mohaiman@gmail.com>
2017-08-11 01:46:22 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+//Check off Todos by clicking 
+$('ul').on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+
+
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+
+$("input[type='text']").keypress(function() {
+    if (event.which === 13) {
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+    }
+});
+
+$(".fa-plus").click(function() {
+    $("input[type='text']").fadeIn()
+});
